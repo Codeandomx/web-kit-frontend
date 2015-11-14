859d63220847f846b0d93801cc50687a68e429c0
Paulo Andrade <source.compu@gmail.com>
2015-11-14 01:26:15 -0600

    Instalamos las dependencias gulp-inject y wiredep para inyectar directamente los archivos js y css propios, ademas de las dependencias instaladas con bower.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 // Importamos las dependencias
 var gulp = require('gulp'),
 	connect = require('gulp-connect'),
-	historyApiFallback = require('connect-history-api-fallback');
+	historyApiFallback = require('connect-history-api-fallback'),
+	inject = require('gulp-inject'),
+	wiredep = require('wiredep').stream;
 
 // Creamos objeto con las rutas utilizadas en gulfile.js
 var paths = {
-	html: './app/**/*.html'
+	html: './app/**/*.html',
+	js: './app/scripts/**/*.js',
+	css: './app/styles/**/*.css',
+	bower: './bower.json'
 };
 
 // Creamos el servidor con la opcion livereload activada y con
@@ -27,9 +32,38 @@ gulp.task('html', function (){
 	.pipe(connect.reload());
 });
 
+// Inyectamos los archivos propios (js y css) al index.html
+gulp.task('inject', function (){
+	var sources = gulp.src([paths.js, paths.css]);
+
+	return gulp.src('index.html', {
+				cwd: './app'
+			})
+			.pipe(inject(sources, {
+				read: false,
+				ignorePath: '/app'
+			}))
+			.pipe(gulp.dest('./app'));
+});
+
+// Inyectamos las dependencias que instalemos con bower
+gulp.task('wiredep', function (){
+	gulp.src('./app/index.html')
+	.pipe(wiredep({
+		directory: './app/vendor',
+		onError: function (err){
+			console.log(err.code);
+		}
+	}))
+	.pipe(gulp.dest('./app'));
+});
+
 // Dejamos en escucha las siguientes tareas
 gulp.task('watch', function (){
 	gulp.watch([paths.html], ['html']);
+	gulp.watch([paths.js], ['inject']);
+	gulp.watch([paths.css], ['inject']);
+	gulp.watch([paths.bower], ['wiredep']);
 });
 
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', ['server', 'html', 'inject', 'wiredep', 'watch']);
